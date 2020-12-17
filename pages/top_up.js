4cd215314fbf1feb524e324e239bcba671571c12
datrine <trinitietp@gmail.com>
2020-12-17 23:51:02 +0100

    first major update

--- pages/top_up.js
+++ pages/top_up.js
@@ -1,10 +1,11 @@
 import { Comp_Register } from '../comps/specials/comp_register';
 import { Header, CompHTMLHeader,Comp_Carousel, Scripts} from "../comps/gen_page/gen_exporter"
+import {Comp_TopUp} from "../comps/specials/comp_top_up"
 export default function TopUp() {
     return <>
     <CompHTMLHeader/>
     <Header/>
-    <Comp_Register/>
+    <Comp_TopUp/>
     <Scripts/>
     </>
 }
